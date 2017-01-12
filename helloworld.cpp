89932038b23dba7f4e1e9f3623b046816a634aa7
Sophie Serrano <smserran@usc.edu>
2017-01-11 17:49:09 -0800

    Fight On!

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,7 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "CS 104 Rules!" << std::endl;
+	std::cout << "Fight On!" << std::endl;
 	
 	return 0;
 }
