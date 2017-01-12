8e8f01cdc3f73b609c2dc34e3587598a5fffd2fa
Sophie Serrano <smserran@usc.edu>
2017-01-11 17:44:27 -0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-int main(int argc, char*argv[])
+int main(int argc, char* argv[])
 {
 	std::cout << "Hello World!" << std::endl;
-
+	std::cout << "CS 104 Rules!" << std::endl;
+	
 	return 0;
 }
